c861efb22ddf1c61193885c3641fbdb98cf8c017
wayofthepie <wayofthepie@gmail.com>
2015-05-29 19:24:52 +0000

    Hostname and session can be passed to image calls

--- src/Docker/Image.hs
+++ src/Docker/Image.hs
@@ -21,39 +21,55 @@ import Network.Wreq
 import qualified Network.Wreq.Session as S
 import Network.Wreq.Types
 
+data DockerClientConfig = DockerClientConfig
+    { hostname :: Host
+    , session  :: S.Session
+    } deriving (Show)
+
+
 
 -- | Create an image.
-createImage :: Postable a => Host -> S.Session -> Options -> Maybe a -> IO (Response ByteString)
-createImage host sess opts maybePayload =
+createImage :: Postable a => DockerClientConfig -> Options -> Maybe a -> IO (Response ByteString)
+createImage conf opts maybePayload =
     case maybePayload of
-        Just p -> post' host sess opts p
-        Nothing-> post' host sess opts emptyObject
+        Just p -> post' conf opts p
+        Nothing-> post' conf opts emptyObject
   where
-    post' host sess opts = S.postWith opts sess (show host  ++ "/images/create")
-
+    post' conf opts =
+        let host = hostname conf
+            sess = session conf
+        in  S.postWith opts sess (show host  ++ "/images/create")
 
 -- | Retrieve info all images cached on the host
-listImages :: Host -> S.Session -> Options -> IO (Response ByteString)
-listImages host sess opts =
-     S.getWith opts sess (show host ++ "/images/json")
+listImages :: DockerClientConfig -> Options -> IO (Response ByteString)
+listImages conf opts =
+    let host = hostname conf
+        sess = session conf
+    in  S.getWith opts sess (show host ++ "/images/json")
 
 
 -- Inspect an image.
-inspectImage :: Host -> S.Session -> Options -> String -> IO (Response ByteString)
-inspectImage host sess opts name =
-    S.getWith opts sess (show host ++ "/images/" ++ name ++ "/json")
+inspectImage :: DockerClientConfig -> Options -> String -> IO (Response ByteString)
+inspectImage conf opts name =
+    let host = hostname conf
+        sess = session conf
+    in  S.getWith opts sess (show host ++ "/images/" ++ name ++ "/json")
 
 
 -- | Get the history of an image.
-imageHistory :: Host -> S.Session -> Options -> String -> IO (Response ByteString)
-imageHistory host sess opts name =
-    S.getWith opts sess (show host ++ "/images/" ++ name ++ "/history")
+imageHistory :: DockerClientConfig -> Options -> String -> IO (Response ByteString)
+imageHistory conf opts name =
+    let host = hostname conf
+        sess = session conf
+    in S.getWith opts sess (show host ++ "/images/" ++ name ++ "/history")
 
 
 -- | Push an image to the registry it is tagged with.
-pushImage :: Host -> S.Session -> Options -> Maybe String -> String -> IO (Response ByteString)
-pushImage host sess opts maybeRepo name =
-    case maybeRepo of
+pushImage :: DockerClientConfig -> Options -> Maybe String -> String -> IO (Response ByteString)
+pushImage conf opts maybeRepo name =
+    let host = hostname conf
+        sess = session conf
+    in case maybeRepo of
         Just repo ->
             S.postWith opts sess (genEndpoint host $ repo ++ "/" ++ name) emptyObject
         Nothing   ->
@@ -64,14 +80,17 @@ pushImage host sess opts maybeRepo name =
 
 
 -- | Tag an images with the given repository name.
-tagImage :: Host -> S.Session -> Options -> String -> IO (Response ByteString)
-tagImage host sess opts name =
-    S.postWith opts sess (show host ++ "/images/" ++ name ++ "/tag") emptyObject
+tagImage :: DockerClientConfig -> Options -> String -> IO (Response ByteString)
+tagImage conf opts name =
+    let host = hostname conf
+        sess = session conf
+    in  S.postWith opts sess (show host ++ "/images/" ++ name ++ "/tag") emptyObject
 
 
 -- | Delete an image.
-deleteImage :: Host -> S.Session -> Options -> String -> IO (Response ByteString)
-deleteImage host sess opts name =
-    S.deleteWith opts sess (show host ++ "/images/" ++ name)
-
+deleteImage :: DockerClientConfig -> Options -> String -> IO (Response ByteString)
+deleteImage conf opts name =
+    let host = hostname conf
+        sess = session conf
+    in S.deleteWith opts sess (show host ++ "/images/" ++ name)
 
