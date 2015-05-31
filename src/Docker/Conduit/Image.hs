54724965480fd84c3cbcaca51f2bfad1fb856af4
wayofthepie <wayofthepie@gmail.com>
2015-05-31 14:53:52 +0100

    Client can now import and image tar

--- src/Docker/Conduit/Image.hs
+++ src/Docker/Conduit/Image.hs
@@ -20,6 +20,8 @@ import Network.URL
 import Docker.Conduit.Types
 
 import Debug.Trace
+
+
 -- | Pull an image of the given name and tag from the specified
 -- repo and registry.
 --
@@ -32,38 +34,79 @@ pullImage queryParams = do
     let host= clientHost config
     let manager = clientManager config
     initReq <- liftIO $ reqBuilder host queryParams
-    http initReq{ method = "POST"
-                , redirectCount = 0
-                , checkStatus = \_ _ _ -> Nothing
-                } manager
+    http initReq manager
   where
     reqBuilder host qps = do
         req <- parseUrl $ (exportHost host) ++ "/images/create"
         return $ setQueryString qps req
+                { method = "POST"
+                , redirectCount = 0
+                , checkStatus = \_ _ _ -> Nothing
+                }
 
--- | Imports and image which was previously exported with 
--- /containers/(containerId)/export. 
+
+-- | Load a tarball with a set of images and tags into docker.
+--
+-- This call does not return any information on the image after
+-- loading.
+loadImagesFromTar :: (MonadReader DockerClientConfig m, MonadResource m)
+    => BL.ByteString
+    -> m (Response (ResumableSource m BS.ByteString))
+loadImagesFromTar tar = do
+    config <- ask
+    let host = clientHost config
+    let manager = clientManager config
+    initReq <- liftIO $ reqBuilder host
+    http (traceShow initReq $ initReq)  manager
+  where
+    reqBuilder host = do
+        req <- parseUrl $ (exportHost host) ++ "/images/load"
+        return $ req { requestBody = requestBodySourceChunked $ CB.sourceLbs tar
+                     , method = "POST"
+                     , redirectCount = 0
+                     , checkStatus = \_ _ _ -> Nothing
+                     }
+
+
+-- |
+-- Endpoint : /images/create
+-- Method   : POST
+--
+-- Imports an image which was previously exported with
+-- /containers/(containerId)/export.
 --
 -- There are two endpoints for importing "images". This endpoint, which
--- imports a tar created from a container export (see /containers/(id)/export) 
+-- imports a tar created from a container export (see /containers/(id)/export)
 -- and the /images/load endpoint which imports a tar of an actual image
 -- (see /images/(name)/get).
-importContainerImageFromTar :: (MonadReader DockerClientConfig m, MonadResource m)
-    => [(BS.ByteString, Maybe BS.ByteString)]
+--
+-- @
+--  ...
+--  tar <- liftIO $ readFile "/var/tmp/containerExport.tar"
+--  ...
+--  importContainerAsImageFromTar
+--      [("tag", Just "1.0.0")]
+--      tar
+--  ...
+-- @
+--
+importContainerAsImageFromTar :: (MonadReader DockerClientConfig m, MonadResource m)
+    => [(BS.ByteString, Maybe BS.ByteString)] -- ^ Needs to be typed as fromSrc is always "-"!
     -> BL.ByteString
     -> m (Response (ResumableSource m BS.ByteString))
-importContainerImageFromTar queryParams tar =do
+importContainerAsImageFromTar queryParams tar =do
     config <- ask
     let host = clientHost config
     let manager = clientManager config
     initReq <- liftIO $ reqBuilder host queryParams tar
-    traceShow initReq$ http initReq { method = "POST"
-                 , redirectCount = 0
-                 , checkStatus = \_ _ _ -> Nothing
-                 } manager
+    traceShow initReq $ http initReq manager
   where
     reqBuilder host qps tar = do
         req <- parseUrl $ (exportHost host) ++ "/images/create"
-        return $ setQueryString qps $
-            req { requestBody =  requestBodySourceChunked $ CB.sourceLbs tar }
+        return $ setQueryString (("fromSrc", Just "-"):qps) $
+            req { requestBody =  requestBodySourceChunked $ CB.sourceLbs tar
+                , method = "POST"
+                , redirectCount = 0
+                , checkStatus = \_ _ _ -> Nothing
+                }
 
